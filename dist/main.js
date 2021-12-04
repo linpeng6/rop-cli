0b106cdef26d7553989a7077e4a621d6d92e56e6
linpeng6 <1204059398@qq.com>
2021-12-04 12:35:33 +0800

    feat 1

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,8 @@ const program = require('commander');
 const create = require('../lib/create');
 
 const chalk = require('chalk');
+
+const figlet = require('figlet');
 /**
  * 创建
  */
@@ -18,6 +20,18 @@ program.version('0.1.0').command('create <name>').option('-f, --force', 'overwri
  */
 
 program.on('--help', () => {
-  console.log(`112${chalk.cyan('abc')}`);
+  // 使用 figlet 绘制 Logo
+  console.log('\r\n' + figlet.textSync('ropcli', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+  }));
+  console.log(`\r\nRun ${chalk.cyan(`rop <command> --help`)} show details\r\n`);
 });
+/**
+ * 解析用户执行命令
+ */
+
 program.parse();
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const program = require('commander');
 const create = require('../lib/create');
 const chalk = require('chalk');
+const figlet = require('figlet');
+
 /**
  * 创建
  */
@@ -9,15 +11,27 @@ program
     .command('create <name>')
     .option('-f, --force', 'overwrite target directory if it exist')
     .description('create a new project')
-    .action((name,options) => {
-        create(name,options)
+    .action((name, options) => {
+        create(name, options)
     })
 
 /**
  * 监听--help
  */
-program.on('--help',()=>{
-    console.log(`112${chalk.cyan('abc')}`)
-})
+program
+    .on('--help', () => {
+        // 使用 figlet 绘制 Logo
+        console.log('\r\n' + figlet.textSync('ropcli', {
+            font: 'Ghost',
+            horizontalLayout: 'default',
+            verticalLayout: 'default',
+            width: 80,
+            whitespaceBreak: true
+        }));
+        console.log(`\r\nRun ${chalk.cyan(`rop <command> --help`)} show details\r\n`)
+    });
 
+/**
+ * 解析用户执行命令
+ */
 program.parse()
